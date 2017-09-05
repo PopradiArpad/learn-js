4973b1f6d72c3f459077f345e718f342cb6f293a
Poprádi Árpád <popradi.arpad11@gmail.com>
2017-09-05 09:18:18 +0200

    fixup! add examples to babel feature object-rest-spread, extend doc for babel feature class-property

--- other/es6/spread_rest.js
+++ other/es6/spread_rest.js
@@ -2,20 +2,38 @@
 const _ = require('lodash');
 
 //rest operator
-//-------------
+//=============
+
+//rest of function parameters
+//----------------------------
 //gathers the rest of the unassigned parameters
 //of a function into an array.
 function rest1(a, b, ...rest) {
   console.assert(a === 1);
   console.assert(b === 2);
+  //the rest is an array
   console.assert(_.isEqual(rest, [3, 4]));
 }
 
 rest1(1, 2, 3, 4);
 
+//rest of object properties (in object destruction)
+//--------------------------------------------------
+//.babelrc
+// {
+//   "plugins":["babel-plugin-transform-object-rest-spread"],
+// }
+//node -r babel-register -r babel-polyfill spread_rest.js
+var {x,y,...z} = {x:1,y:2,a:3,b:4}
+//the rest is an object
+console.assert(_.isEqual(z, {a:3,b:4} ));
+
+
 //spread operator
-//-------------------
-//spreads out any iterable
+//==================
+
+//spread any iterable
+//------------------
 //(object with the standard iterator ([Symbol.iterator]))
 let a1 = [2, 3];
 console.assert(_.isEqual([1, ...a1, 4], [1, 2, 3, 4]));
@@ -34,7 +52,8 @@ console.assert(_.isEqual(['_', ...g1(), '_'], ['_',1,2,'_']));
 //In function call it spreads the iterables into parameters
 rest1(...[1,2,3,4]);
 
-//----------------------------
+//spread object properties
+//------------------------
 //.babelrc
 // {
 //   "plugins":["babel-plugin-transform-object-rest-spread"],
@@ -47,5 +66,11 @@ var state = {
   }
 };
 
-console.log({example1: { value: 'count()', value: 'newvalue' } });
-console.log({example1: { ...state.example1, value: 'newvalue',value2: 2 } });
+console.assert(_.isEqual(
+  {
+    example1: { ...state.example1, value: 'newvalue',value2: 2 }
+  },
+  {
+    example1: {value: 'newvalue',value2: 2 }
+  }
+));
