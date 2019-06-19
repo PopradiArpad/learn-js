67c8d641c01b4b77bf51d24076352f584e698251
Poprádi Árpád <popradi.arpad11@gmail.com>
2019-06-19 12:12:08 +0200

    About the this and error handling and events.

--- paradigms/events.js
+++ paradigms/events.js
@@ -13,10 +13,16 @@ const eventEmitter = new events.EventEmitter();
 /*
  * Ugyanúgy egészítők adhatók át.
  */
-eventEmitter.on("event1", (arg) => {
+eventEmitter.on("event1", function(arg) {
   console.log(`function1: event1 happened with ${arg}`);
   /*
-    Ellenben a visszatérési érték semmibe vétetik.
+    Ellenben a this az értesítő!
+  */
+  if (this !== eventEmitter) {
+    throw new Error("this !== eventEmitter");
+  }
+  /*
+    Ellenben a visszatérési érték semmibe vétetik!
   */
   return "huhu1";
 });
@@ -41,3 +47,32 @@ eventEmitter.removeListener("event1", function2);
 console.log("Before event emitting");
 eventEmitter.emit("event1", "hi!");
 console.log("After event emitting");
+
+
+/*
+  Hiba kezelés:
+
+  Hiba nem maradhat kezeletlen!
+*/
+let errorCame = false;
+try {
+  // Az "error" értesítés hibaként tartatik számon a Node-ban.
+  eventEmitter.emit("error");
+} catch (e) {
+  // Hibakezelő csin nem léte kezeletlen hibát jelent! ERR_UNHANDLED_ERROR
+  errorCame = true;
+}
+if (! errorCame) {
+  throw new Error("! errorCame");
+}
+
+errorCame = false;
+eventEmitter.on("error", ()=>{});
+try {
+  eventEmitter.emit("error");
+} catch (e) {
+  errorCame = true;
+}
+if (errorCame) {
+  throw new Error("errorCame");
+}
